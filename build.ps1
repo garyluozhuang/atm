8d5d95ca7058230f86d1872ef43ff52ffd715dc8
garyluozhuang <luozhuangswift@gmail.com>
2024-04-27 14:23:10 -0400

    final exam

--- build.ps1
+++ build.ps1
@@ -8,12 +8,9 @@ dotnet build -c Debug
 dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
 # Generate the documentation
 doxygen Doxyfile
-git config --global user.email "zluo3@wpi.edu"
-git config --global user.name "garyluozhuang"
-cd docs
-git add --all
-git commit -m "Update documentation"
-git push -u origin main
+
+
+
 
 
 
